71b3523c5b9d89b2d287e8c62424c5828c6988ee
usrmrz <usrmrz@gmail.com>
2024-09-03 04:18:10 +0300

    add icons, bars and floating act button

--- app/src/main/java/dev/usrmrz/scaffold/MainActivity.kt
+++ app/src/main/java/dev/usrmrz/scaffold/MainActivity.kt
@@ -6,21 +6,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dev.usrmrz.scaffold.ui.theme.ScaffoldTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,47 +47,125 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppScreen() {
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
-            TopAppBar(
-                colors = topAppBarColors(
+            CenterAlignedTopAppBar(
+                actions = {
+                    IconButton(
+                        onClick = { /*TODO*/ }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.DateRange,
+                            contentDescription = ("Events")
+                        )
+                    }
+                    IconButton(
+                        onClick = { /*TODO*/ }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Create,
+                            contentDescription = ("Edit Event")
+                        )
+                    }
+                    IconButton(
+                        onClick = { /*TODO*/ }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Settings,
+                            contentDescription = ("Settings")
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.largeTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.primary,
                 ),
+                navigationIcon = {
+                    IconButton(
+                        onClick = { /*TODO*/ }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Menu,
+                            contentDescription = ("Main Menu")
+                        )
+                    }
+                },
                 title = {
                     Text(
-                        text = "Top app bar")
-                })
+                        text = ("Scaffold App"),
+                        fontWeight = FontWeight.Bold,
+                    )
+                },
+            )
         },
 
         bottomBar = {
-
+            NavigationBar {
+                NavigationBarItem(
+                    selected = true,
+                    label = { Text("Items") },
+                    onClick = { /*TODO*/ },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.List,
+                            contentDescription = ("Items")
+                        )
+                    }
+                )
+                NavigationBarItem(
+                    selected = false,
+                    label = { Text("Settings") },
+                    onClick = { /*TODO*/ },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Default.Settings,
+                            contentDescription = ("Settings")
+                        )
+                    }
+                )
+                NavigationBarItem(
+                    selected = false,
+                    label = { Text("Profile") },
+                    onClick = { /*TODO*/ },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Default.AccountBox,
+                            contentDescription = ("Profile")
+                        )
+                    }
+                )
+            }
         },
 
         floatingActionButton = {
-
+            FloatingActionButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = ("Add New Item")
+                )
+            }
         },
 
         floatingActionButtonPosition = FabPosition.End,
 
         snackbarHost = {}
 
-    ) { innerPadding ->
-        Column(
+    ) { paddingValues ->
+        Text(
             modifier = Modifier
-                .padding(innerPadding),
-            verticalArrangement = Arrangement.spacedBy(6.dp),
-        ) {
-            Text(
-                modifier = Modifier.padding(20.dp),
-                text = "Hello Everyone!"
-            )
-
-        }
-
+                .padding(paddingValues)
+                .fillMaxSize()
+                .wrapContentHeight(),
+            text = "Hello Everyone!",
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
     }
 }
 
